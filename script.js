45a1d1289255bc83b3c482e2ac0391420e05aac0
Mario D Alvarez <mario.alvarez.san@gmail.com>
2022-05-03 17:31:23 -0400

    Final
    -Background and text change dynamically with color input.
    -Background css styling copied to clipboard on click.

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 var css = document.querySelector("h3");
-var color1 = document.querySelector("color1");
-var color2 = document.querySelector("color2");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var cssDiv = document.querySelector("div");
+var toolTip = document.querySelector("span");
+
+css.textContent = `background: linear-gradient(to right, ${color1.value}, ${color2.value});`;
 
 function setGradient() {
-    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+
+    css.textContent = `background: ${body.style.background};`;
 }
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+cssDiv.addEventListener("click", () => {
+    toolTip.innerHTML = "Copied!"
+    navigator.clipboard.writeText(`${css.textContent}`);
+})
+
+cssDiv.addEventListener("mouseleave", () => {
+    toolTip.innerHTML = "Click to copy!";
+})
